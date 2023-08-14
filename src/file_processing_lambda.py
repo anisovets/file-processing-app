2e45bcb197e834e675b64ef93c186f1d0f49a36c
Yaru Anisovec <anisovets@gmail.com>
2023-08-14 12:46:24 -0400

    adedd loggeing support

--- src/file_processing_lambda.py
+++ src/file_processing_lambda.py
@@ -1,2 +1,12 @@
+import boto3
+import logging
+
 def file_upload_handler(event, context):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.info('Lambda function has been triggered!')
+
+    s3Client = boto3.client('s3')
+
+    
     return "File processing handler!"
