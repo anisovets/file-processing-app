f8aa75fa4143565aef4fb1845f26eba0e8245d74
Yaru Anisovec <anisovets@gmail.com>
2023-08-14 13:36:22 -0400

    adjusted log-level

--- src/file_processing_lambda.py
+++ src/file_processing_lambda.py
@@ -3,8 +3,8 @@ import logging
 
 def file_upload_handler(event, context):
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    logger.info('Lambda function has been triggered!')
+    logger.setLevel(logging.DEBUG)
+    logger.debug('Lambda function has been triggered!')
 
     s3Client = boto3.client('s3')
 
